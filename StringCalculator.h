b4b2f87ad1abd0600781fe56afbc7f313704e108
SmithaLMagge <smithamagge@gmail.com>
2024-06-27 17:41:40 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,7 +9,7 @@ private:
  int splitStringcheckforDelimiter(const std::string& str);
  int checkfornumbers(const std::string& str);
  int checkForNumbers(const std::string& str);
- int bool isValidNumber(const std::string& str);
+ bool isValidNumber(const std::string& str);
 public:
 int add(const std::string& str);
 };
