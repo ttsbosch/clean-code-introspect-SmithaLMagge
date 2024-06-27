7f1127c826cd8d60f9dc38232f7a4d980b6be9dd
SmithaLMagge <smithamagge@gmail.com>
2024-06-27 16:22:46 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,12 +1,41 @@
 #include "StringCalculator.h"
 int StringCalculator :: add(string str)
 {
+  uint sum =0;
   if(str.empty() || str == "0")
   {
     return 0;
   }
   else
   {
-    return 1;
+    sum = sumIntegersInString(str);
+    return ;
   }
+}
+
+int StringCalculator :: sumIntegersInString(string str)
+{
+   int sum = 0;
+    std::istringstream ss(str);
+    std::string token;
+
+    while (getline(str, token, ',')) { // Split by any delimiter (here: ',')
+        std::istringstream tokenStream(token);
+        std::string numStr;
+
+        while (tokenStream >> numStr) { // Split by whitespace (including spaces, tabs, newlines)
+            bool isValidNumber = true;
+            for (char c : numStr) {
+                if (!std::isdigit(c)) {
+                    isValidNumber = false;
+                    break;
+                }
+            }
+            if (isValidNumber) {
+                sum += std::stoi(numStr);
+            }
+        }
+    }
+
+    return sum;
 }
