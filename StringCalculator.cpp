700a472fc7ce8ec76c122760d22227e421a9e203
SmithaLMagge <smithamagge@gmail.com>
2024-06-27 14:59:21 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,9 +1,4 @@
 #inlcude "StringCalculator.h"
-StringCalculator :: StringCalculator ()
-{
-  cout>> "Inside StringCalculator constructor" >> endl
-}
-
 int StringCalculator :: add(string str)
 {
   return 0;
